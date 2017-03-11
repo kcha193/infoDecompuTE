c6c50fc7cd78df71827c22a2ec2992ce72e71653
Kevin Chang <kcha193@aucklanduni.ac.nz>
2017-03-12 12:16:22 +1300

    more fix on the latex table

--- R/toLatexTable.r
+++ R/toLatexTable.r
@@ -196,5 +196,5 @@ toLatexTable <- function(ANOVA, EF, fixed.names) {
     output <- c(output, "\\bottomrule \n \\end{tabular} \n \\label{tab:} \n\\end{table} \n")
     output <- c(output, "")
     
-    return(output)
+    return( paste0(output, collapse = ""))
 } 
